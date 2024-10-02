b9ed95359aecd5e8e4cbda28fd2ae56d0db91631
Ciel <vivpiere@gmail.com>
2024-10-02 11:38:19 +0200

    TP 2

--- src/fr/declaration/variable/DeclarationApp.java
+++ src/fr/declaration/variable/DeclarationApp.java
@@ -1,4 +1,21 @@
 package fr.declaration.variable;
 
 public class DeclarationApp {
+    public static void main(String[] args) {
+        byte b = 24;
+        short sh = 30000;
+        int integer = 5;
+        long l = 100L;
+        float f = 5.75F;
+        double d = 1.99;
+        char c = '@';
+        boolean bool = true;
+        String str = "Bonjour";
+
+        System.out.println(str);
+
+        String randomString = "Voici le résultat d’un calcul : \n 1+5=6";
+
+        System.out.println(randomString);
+    }
 }
